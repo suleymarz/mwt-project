03abff68ec58e7d7e701a9b7441b2743e34caae3
Wendy Gomez <suleymarz@outlook.com>
2021-11-20 17:51:21 -0600

    Add test data arrays

---
+++ src/test/test-arrays.ts
@@ -0,0 +1,5 @@
+import { NodeT } from '../types'
+
+export const testArray1: NodeT = ['a', ['b'], ['c']]
+export const testArray2 = [1, [2], [3, null, [5]]]
+export const testArray3 = [10, [5, [3], [7]], [20, null, [30]]]
